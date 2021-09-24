eb48b62931ddd7ebe3ccb76a240c2afdc1160a3b
Mohamed Darwish <msdarwish.2000@outlook.com>
2021-09-24 06:55:27 +0200

    Add publishing script

--- lrucache/build.gradle.kts
+++ lrucache/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java-library")
     id("kotlin")
+
+    id("maven-publish")
 }
 
 java {
@@ -10,4 +12,37 @@ java {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles sources JAR"
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").java.srcDirs)
+}
+
+artifacts {
+    archives(sourcesJar)
+}
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                setUrl("https://maven.pkg.github.com/Mayaka-Apps/Kotlin-LruCache")
+            }
+        }
+
+        publications {
+            create<MavenPublication>("library") {
+                groupId = "com.mayakapps.lrucache"
+                artifactId = "lrucache"
+                version = "1.0.0"
+
+                from(components["java"])
+                artifact(sourcesJar)
+            }
+        }
+    }
 }
