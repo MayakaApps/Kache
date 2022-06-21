4e8376b0bec8e90fcb08cd58d0480a2fffd05799
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-21 15:43:10 +0200

    Update Gradle to v7.4.2, target JVM to 1.8, and Project JVM to 17

--- lrucache/build.gradle.kts
+++ lrucache/build.gradle.kts
@@ -6,47 +6,10 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles sources JAR"
-    archiveClassifier.set("sources")
-    from(sourceSets.getByName("main").java.srcDirs)
-}
-
-artifacts {
-    archives(sourcesJar)
-}
-
-afterEvaluate {
-    publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                setUrl("https://maven.pkg.github.com/Mayaka-Apps/Kotlin-LruCache")
-                credentials {
-                    username = project.properties["GITHUB_USER"] as String
-                    password = project.properties["GITHUB_PERSONAL_ACCESS_TOKEN"] as String
-                }
-            }
-        }
-
-        publications {
-            create<MavenPublication>("library") {
-                groupId = "com.mayakapps.lrucache"
-                artifactId = "lrucache"
-                version = "1.0.0"
-
-                from(components["java"])
-                artifact(sourcesJar)
-            }
-        }
-    }
 }
