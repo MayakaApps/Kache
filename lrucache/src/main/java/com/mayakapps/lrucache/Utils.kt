52314abb35d37c6c69f68fadf507e46329ffa7cf
Mohamed Darwish <msdarwish.2000@outlook.com>
2022-06-21 21:20:48 +0200

    Refactor journal file

--- lrucache/src/main/java/com/mayakapps/lrucache/Utils.kt
+++ lrucache/src/main/java/com/mayakapps/lrucache/Utils.kt
@@ -1,5 +1,7 @@
 package com.mayakapps.lrucache
 
+import java.io.DataInputStream
+import java.io.DataOutputStream
 import java.io.File
 import java.io.IOException
 
@@ -11,6 +13,19 @@ internal fun File.renameToOrThrow(dest: File, deleteDest: Boolean) {
 internal fun File.deleteOrThrow() {
     if (exists() && !delete()) throw IOException()
 }
+
+
+internal fun DataOutputStream.writeLengthString(string: String) {
+    writeByte(string.length)
+    writeString(string)
+}
+
+internal fun DataOutputStream.writeString(string: String) = write(string.encodeToByteArray())
+
+internal fun DataInputStream.readString() = readString(readByte().toInt())
+internal fun DataInputStream.readString(length: Int) = readBytes(length).decodeToString()
+internal fun DataInputStream.readBytes(count: Int) = ByteArray(count).also { read(it) }
+
 
 internal fun ByteArray.toHexString(): String =
     buildString(size * 2) {
