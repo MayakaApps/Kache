da18bf19ba57613ceb0f5b477a724bd612978aa3
Mohamed Darwish <msdarwish@mayakapps.com>
2023-02-04 00:56:53 +0200

    Revert "Move JournalReader and JournalWriter to common source set with dummy apple implementation"
    This reverts commit 57c58d67

--- lrucache/src/commonMain/kotlin/com/mayakapps/lrucache/journal/JournalReader.kt
+++
@@ -1,9 +0,0 @@
-package com.mayakapps.lrucache.journal
-
-import com.mayakapps.lrucache.io.Closeable
-
-internal expect class JournalReader(path: String) : Closeable {
-    val isCorrupted: Boolean
-
-    fun readFully(): List<JournalOp>
-}
