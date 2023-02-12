5e97d305a7d44ea7b98666d844f2aa9bbf71481f
Mohamed Darwish <msdarwish@mayakapps.com>
2023-02-12 02:41:54 +0200

    Remove recently deprecated targets
    This is according to guidelines mentioned here: https://kotlinlang.org/docs/native-target-support.html#deprecated-targets

--- lru-cache/build.gradle.kts
+++ lru-cache/build.gradle.kts
@@ -28,11 +28,9 @@ kotlin {
     macosArm64(appleConfig)
 
     ios(appleConfig)
-    iosArm32(appleConfig)
     iosSimulatorArm64(appleConfig)
 
     watchos(appleConfig)
-    watchosX86(appleConfig)
     watchosSimulatorArm64(appleConfig)
 
     tvos(appleConfig)
@@ -88,41 +86,33 @@ kotlin {
             macosArm64Test.dependsOn(this)
         }
 
-        val iosArm32Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by getting {
             dependsOn(appleMain)
 
             // iOS target shortcut only contains: iosArm64, iosX64
-            iosArm32Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
 
-        val iosArm32Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by getting {
             dependsOn(appleTest)
 
-            iosArm32Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
 
-        val watchosX86Main by getting
         val watchosSimulatorArm64Main by getting
         val watchosMain by getting {
             dependsOn(appleMain)
 
             // watchOS target shortcut only contains: watchosArm32, watchosArm64, watchosX64
-            watchosX86Main.dependsOn(this)
             watchosSimulatorArm64Main.dependsOn(this)
         }
 
-        val watchosX86Test by getting
         val watchosSimulatorArm64Test by getting
         val watchosTest by getting {
             dependsOn(appleTest)
 
-            watchosX86Test.dependsOn(this)
             watchosSimulatorArm64Test.dependsOn(this)
         }
 
