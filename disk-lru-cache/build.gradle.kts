1254ad5ff8e2b925889035fa5c666154262222a2
Mohamed Darwish <msdarwish@mayakapps.com>
2023-02-12 04:16:34 +0200

    Add JS, Linux, and Windows targets to LruCache and remove deprecated (#21)
    * Remove recently deprecated targets
    
    This is according to guidelines mentioned here: https://kotlinlang.org/docs/native-target-support.html#deprecated-targets
    
    * Add support for JS, Linux, and Windows to LruCache

--- disk-lru-cache/build.gradle.kts
+++ disk-lru-cache/build.gradle.kts
@@ -28,7 +28,6 @@ kotlin {
     macosArm64(appleConfig)
 
     ios(appleConfig)
-    iosArm32(appleConfig)
     iosSimulatorArm64(appleConfig)
 
     watchos(appleConfig)
@@ -86,41 +85,33 @@ kotlin {
             macosArm64Test.dependsOn(this)
         }
 
-        val iosArm32Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by getting {
             dependsOn(appleMain)
 
             // iOS target shortcut only contains: iosArm64, iosX64
-            iosArm32Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
 
-        val iosArm32Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by getting {
             dependsOn(appleTest)
 
-            iosArm32Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
 
-        val watchosX86Main by getting
         val watchosSimulatorArm64Main by getting
         val watchosMain by getting {
             dependsOn(appleMain)
 
             // watchOS target shortcut only contains: watchosArm32, watchosArm64, watchosX64
-            watchosX86Main.dependsOn(this)
             watchosSimulatorArm64Main.dependsOn(this)
         }
 
-        val watchosX86Test by getting
         val watchosSimulatorArm64Test by getting
         val watchosTest by getting {
             dependsOn(appleTest)
 
-            watchosX86Test.dependsOn(this)
             watchosSimulatorArm64Test.dependsOn(this)
         }
 
--- lru-cache/build.gradle.kts
+++ lru-cache/build.gradle.kts
@@ -16,6 +16,18 @@ kotlin {
         withJava()
     }
 
+    js(IR) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+
+        nodejs()
+    }
+
     val appleConfig: KotlinNativeTarget.() -> Unit = {
         binaries {
             framework {
@@ -28,15 +40,33 @@ kotlin {
     macosArm64(appleConfig)
 
     ios(appleConfig)
-    iosArm32(appleConfig)
     iosSimulatorArm64(appleConfig)
 
     watchos(appleConfig)
-    watchosX86(appleConfig)
     watchosSimulatorArm64(appleConfig)
+    // Not supported by Coroutines
+    // Issue: https://github.com/Kotlin/kotlinx.coroutines/issues/3601
+    // watchosDeviceArm64(appleConfig)
 
     tvos(appleConfig)
     tvosSimulatorArm64(appleConfig)
+
+    linuxX64()
+    // Not supported by Coroutines
+    // Issue: https://github.com/Kotlin/kotlinx.coroutines/issues/855
+    // linuxArm64()
+
+    mingwX64()
+
+    // Not supported by Coroutines
+    // Issue: https://github.com/Kotlin/kotlinx.coroutines/issues/812
+    // androidNativeArm32()
+    // androidNativeArm64()
+    // androidNativeX86()
+    // androidNativeX64()
+
+    // Still experimental and not supported by dependencies
+    // wasm()
 
     @Suppress("UNUSED_VARIABLE")
     sourceSets {
@@ -55,7 +85,7 @@ kotlin {
             }
         }
 
-        val appleMain by creating {
+        val nonJvmMain by creating {
             dependsOn(commonMain)
 
             dependencies {
@@ -63,9 +93,25 @@ kotlin {
             }
         }
 
-        val appleTest by creating {
+        val nonJvmTest by creating {
             dependsOn(commonTest)
-            dependsOn(appleMain)
+            dependsOn(nonJvmMain)
+        }
+
+        val jsMain by getting {
+            dependsOn(nonJvmMain)
+        }
+
+        val jsTest by getting {
+            dependsOn(nonJvmTest)
+        }
+
+        val appleMain by creating {
+            dependsOn(nonJvmMain)
+        }
+
+        val appleTest by creating {
+            dependsOn(nonJvmTest)
         }
 
         val macosX64Main by getting
@@ -88,41 +134,33 @@ kotlin {
             macosArm64Test.dependsOn(this)
         }
 
-        val iosArm32Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by getting {
             dependsOn(appleMain)
 
             // iOS target shortcut only contains: iosArm64, iosX64
-            iosArm32Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
 
-        val iosArm32Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by getting {
             dependsOn(appleTest)
 
-            iosArm32Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
 
-        val watchosX86Main by getting
         val watchosSimulatorArm64Main by getting
         val watchosMain by getting {
             dependsOn(appleMain)
 
             // watchOS target shortcut only contains: watchosArm32, watchosArm64, watchosX64
-            watchosX86Main.dependsOn(this)
             watchosSimulatorArm64Main.dependsOn(this)
         }
 
-        val watchosX86Test by getting
         val watchosSimulatorArm64Test by getting
         val watchosTest by getting {
             dependsOn(appleTest)
 
-            watchosX86Test.dependsOn(this)
             watchosSimulatorArm64Test.dependsOn(this)
         }
 
@@ -139,6 +177,22 @@ kotlin {
             dependsOn(appleTest)
 
             tvosSimulatorArm64Test.dependsOn(this)
+        }
+
+        val linuxX64Main by getting {
+            dependsOn(nonJvmMain)
+        }
+
+        val linuxX64Test by getting {
+            dependsOn(nonJvmTest)
+        }
+
+        val mingwX64Main by getting {
+            dependsOn(nonJvmMain)
+        }
+
+        val mingwX64Test by getting {
+            dependsOn(nonJvmTest)
         }
     }
 }
