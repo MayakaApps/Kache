418fea80238644ad5bf71f5a1b1143866a951093
Mohamed Darwish <msdarwish@mayakapps.com>
2023-04-13 01:01:40 +0200

    Fix LRU strategy on non-JVM platforms

--- kache/src/nonJvmMain/kotlin/com/mayakapps/kache/KacheStrategyMap.kt
+++ kache/src/nonJvmMain/kotlin/com/mayakapps/kache/KacheStrategyMap.kt
@@ -1,7 +1,7 @@
 package com.mayakapps.kache
 
 actual fun <K : Any, V : Any> getMapByStrategy(strategy: KacheStrategy): MutableMap<K, V> = when (strategy) {
-    KacheStrategy.LRU -> LinkedHashMap(0, 0.75F)
+    KacheStrategy.LRU -> AccessOrderedMap(LinkedHashMap(0, 0.75F))
     KacheStrategy.MRU -> TODO()
     KacheStrategy.FIFO -> LinkedHashMap(0, 0.75F)
     KacheStrategy.FILO -> TODO()
