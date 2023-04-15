b5e64b29fb6ac0f6eaaec1eafed2eca4853f3bfa
Mohamed Darwish <msdarwish@mayakapps.com>
2023-04-15 23:16:23 +0200

    Bump journal version to 2

--- file-kache-okio/src/commonMain/kotlin/com/mayakapps/kache/journal/Consts.kt
+++ file-kache-okio/src/commonMain/kotlin/com/mayakapps/kache/journal/Consts.kt
@@ -1,7 +1,7 @@
 package com.mayakapps.kache.journal
 
 internal const val JOURNAL_MAGIC = "JOURNAL"
-internal const val JOURNAL_VERSION: Byte = 1
+internal const val JOURNAL_VERSION: Byte = 2
 
 internal const val JOURNAL_FILE = "journal"
 internal const val JOURNAL_FILE_TEMP = "$JOURNAL_FILE.tmp"
