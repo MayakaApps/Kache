efba626c262a16c13fd46b71c94f70938e21c450
Mohamed Darwish <msdarwish@mayakapps.com>
2023-04-16 07:55:16 +0200

    Replace old API-only documentation with mkdocs-based dummy documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
 
-    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka)
     alias(libs.plugins.mavenPublish) apply false
 }
 
@@ -9,4 +9,19 @@ allprojects {
     repositories {
         mavenCentral()
     }
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>().configureEach {
+    outputDirectory.set(file("$rootDir/docs/api"))
+
+    moduleName.set("Kache")
+    pluginsMapConfiguration.set(
+        mapOf(
+            "org.jetbrains.dokka.base.DokkaBase" to """
+                {
+                    "footerMessage": "Copyright &copy; 2023 MayakaApps."
+                }
+            """
+        )
+    )
 }
