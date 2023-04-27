0cb87289c4f37cb83171b9bd002db0ddb315e5ee
Mohamed Darwish <msdarwish@mayakapps.com>
2023-04-27 10:22:43 +0200

    Optimize imports

--- file-kache-base/src/commonMain/kotlin/com/mayakapps/kache/PlatformVariables.kt
+++ file-kache-base/src/commonMain/kotlin/com/mayakapps/kache/PlatformVariables.kt
@@ -1,10 +1,7 @@
 package com.mayakapps.kache
 
 import kotlinx.coroutines.CoroutineDispatcher
-import okio.Closeable
 import okio.FileSystem
-import okio.Path
-import okio.use
 
 internal expect val defaultFileSystem: FileSystem
 
