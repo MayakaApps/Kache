f7cb0f4c0472b5ecf6d4182b3df180840b6ccc25
Mohamed Darwish <msdarwish@mayakapps.com>
2023-04-29 03:03:13 +0300

    Add documentation for FileKache classes

--- file-kache/src/commonMain/kotlin/com/mayakapps/kache/FileKache.kt
+++ file-kache/src/commonMain/kotlin/com/mayakapps/kache/FileKache.kt
@@ -6,6 +6,23 @@ import kotlinx.coroutines.async
 import okio.Path
 import okio.Path.Companion.toPath
 
+/**
+ * A persistent coroutine-safe [ContainerKache] implementation that uses [Okio](https://square.github.io/okio/) to
+ * store files under the hood and exposes a simple path-based API.
+ *
+ * It uses a journal file to keep track of the cache state and to ensure that the cache is always in a consistent state.
+ *
+ * It can be built using the following syntax:
+ * ```
+ * val cache = OkioFileKache(directoryPath = "cache", maxSize = 100L * 1024L * 1024L) {
+ *     strategy = KacheStrategy.LRU
+ *     // ...
+ * }
+ * ```
+ *
+ * @see Configuration
+ * @see invoke
+ */
 class FileKache private constructor(
     private val baseKache: ContainerKache<String, Path>,
     private val creationScope: CoroutineScope,
@@ -43,16 +60,51 @@ class FileKache private constructor(
     override suspend fun close() =
         baseKache.close()
 
+    /**
+     * Configuration for [FileKache]. It is used as a receiver of [FileKache] builder which is [invoke].
+     */
     data class Configuration(
+
+        /**
+         * The path of the directory where the cache files and the journal are stored.
+         */
         var directoryPath: String,
+
+        /**
+         * The maximum size of the cache in bytes.
+         */
         var maxSize: Long,
+
+        /**
+         * The strategy used to evict entries from the cache.
+         */
         var strategy: KacheStrategy = KacheStrategy.LRU,
+
+        /**
+         * The coroutine dispatcher used for executing `creationFunction` of put requests.
+         */
         var creationScope: CoroutineScope = CoroutineScope(ioDispatcher),
+
+        /**
+         * The version of the cache. This is useful to invalidate the cache when the format of the data stored in the
+         * cache changes.
+         */
         var cacheVersion: Int = 1,
+
+        /**
+         * The [KeyTransformer] used to transform the keys before they are used to store and retrieve data. It is
+         * needed to avoid using invalid characters in the file names.
+         */
         var keyTransformer: KeyTransformer? = SHA256KeyHasher,
     )
 
     companion object {
+        /**
+         * Creates a new [FileKache] instance with the given [directoryPath] and [maxSize] and is configured by
+         * [configuration].
+         *
+         * @see Configuration
+         */
         suspend operator fun invoke(
             directoryPath: String,
             maxSize: Long,
