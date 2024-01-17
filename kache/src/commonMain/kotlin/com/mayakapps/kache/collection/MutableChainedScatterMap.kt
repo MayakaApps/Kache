19b73f7cfa4b996b7583e4f717988b724cfafea7
Mohamed Darwish <msdarwish@mayakapps.com>
2024-01-17 02:40:15 +0200

    Remove unused import
    Signed-off-by: Mohamed Darwish <msdarwish@mayakapps.com>

--- kache/src/commonMain/kotlin/com/mayakapps/kache/collection/MutableChainedScatterMap.kt
+++ kache/src/commonMain/kotlin/com/mayakapps/kache/collection/MutableChainedScatterMap.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 MayakaApps
+ * Copyright 2024 MayakaApps
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package com.mayakapps.kache.collection
 
-import androidx.collection.*
 import kotlin.jvm.JvmField
 
 internal class MutableChainedScatterMap<K, V>(
